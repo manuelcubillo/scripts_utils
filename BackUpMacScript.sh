1cf0d4d508ed3b68e2c70a7e3c5e2279eaa39f9c
manucubillo <manuel.cubillo1@gmail.com>
2023-10-29 00:05:22 +0200

    back up important files from mac to external device

---
+++ BackUpMacScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+rsync -varh --stats --progress  /Users/manuelcubillo/Projects /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress /Users/manuelcubillo/Box Sync  /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress  /Users/manuelcubillo/Documents /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress  /Applications /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress  /Users/manuelcubillo/Pictures /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress  /Users/manuelcubillo/Music /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress  /Users/manuelcubillo/Movies /Volumes/CORSAIR-GTX/MacManuelCubillo
+rsync -varh --stats --progress /Users/manuelcubillo/certs  /Volumes/CORSAIR-GTX/MacManuelCubillo
+#rsync -varhn --stats --progress  /Users/manuelcubillo/Desktop /Volumes/CORSAIR-GTX/MacManuelCubillo
